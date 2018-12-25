099f42629064b651f7ebc984dbd901be4ecb18cc
Maxim Oransky <maxim.oransky@gmail.com>
2018-12-25 22:41:57 +0300

    модель для фестивалей и много комментариев

--- demobaza/models.py
+++ demobaza/models.py
@@ -4,6 +4,22 @@ from pytils.translit import slugify  # slugify() из джанги не знае
 
 
 class User(AbstractUser):
+    # Один пользователь может управлять несколькими музыкальными проектами
+    # например, играет с группой, играет сольно. А может это просто зритель,
+    # тогда у него ноль проектов.
+    #
+    # Также одним проектом могут управлять несколько пользователей. Создать
+    # новый проект может любой пользователь. Если надо доверить управление
+    # проектом ещё кому-то, это должен явно сделать тот, у кого уже есть доступ.
+    # Ролей пока не делаем, то есть все управляющие равны. Если солист Вася
+    # доверил басисту Пете управлять страницей группы «Бетонные сырники»,
+    # а басист Петя отключил солисту Васе управление (вот сволочь), то это
+    # их личные проблемы, пусть договариваются между собой сами
+    #
+    # Если проектом управляет только один человек, он не может отказаться
+    # от управления.
+    #
+    # Если группу
     musicians = models.ManyToManyField(
         'demobaza.Musician',
         related_name='users',
@@ -11,31 +27,35 @@ class User(AbstractUser):
         blank=True,
     )
 
-    class Meta:
+    class Meta(AbstractUser.Meta):
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
 
 
 class Musician(models.Model):
-    enabled = models.BooleanField('включён', default=True)
-    verified = models.DateTimeField('проверен администрацией', null=True)
     created = models.DateTimeField('создан', auto_now_add=True)
+    # Архивных не показываем в списках, но показываем по прямой ссылке.
+    # должна быть плашка «архив». Треки у архивных не отображаются, только текст
+    archive = models.BooleanField('архив', default=False)
+    # Если не проверен, отображаем так же точно. А если проверен, можно
+    # писать «проверен»
+    verified = models.DateTimeField('проверен администрацией', null=True)
     name = models.CharField('название', max_length=100, unique=True)
     slug = models.SlugField(editable=False, unique=True, db_index=True)
     short_text = models.TextField('короткий текст', blank=True)
     long_text = models.TextField('длинный текст', blank=True)
     city = models.ForeignKey(
-        'demobaza.Genre',
+        'demobaza.City',
         null=True,
         verbose_name='город',
         on_delete=models.PROTECT,
-        related_name='city_musicians',
+        related_name='musicians',
     )
     genres = models.ManyToManyField(
         'demobaza.Genre',
         blank=True,
         verbose_name='жанры',
-        related_name='genre_musicians',
+        related_name='musicians',
     )
 
     class Meta:
@@ -83,6 +103,35 @@ class City(models.Model):
     class Meta:
         verbose_name = 'город'
         verbose_name_plural = 'города'
+        ordering = (
+            'name',
+        )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.strip()
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Festival(models.Model):
+    name = models.CharField('название', max_length=50, unique=True)
+    slug = models.SlugField(editable=False, unique=True, db_index=True)
+    short_text = models.TextField('короткий текст', blank=True)
+    long_text = models.TextField('длинный текст', blank=True)
+    city = models.ForeignKey(
+        'demobaza.City',
+        null=True,
+        verbose_name='город',
+        on_delete=models.PROTECT,
+        related_name='festivals',
+    )
+
+    class Meta:
+        verbose_name = 'фестиваль'
+        verbose_name_plural = 'фестивали'
         ordering = (
             'name',
         )
