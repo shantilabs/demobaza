357234c2201f2eee175c5eac37f506339e51db6d
Maxim Oransky <maxim.oransky@gmail.com>
2018-12-26 23:09:02 +0300

    favicon.ico fix

--- demobaza/urls.py
+++ demobaza/urls.py
@@ -1,22 +1,26 @@
+import os
+
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
-urlpatterns = [
+urlpatterns = []
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.views.static import serve
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL)
+    urlpatterns += [
+        re_path('^(?P<path>[^/]+)$', serve, kwargs={
+            'document_root': os.path.join(settings.BASE_DIR, 'web'),
+            'show_indexes': True,
+        }),
+    ]
+
+urlpatterns += [
     path('', views.home),
     path('admin/', admin.site.urls),
 ]
-
-
-if settings.DEBUG:
-    import os
-    from django.conf.urls.static import static
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    web_root = os.path.join(settings.BASE_DIR, 'web')
-    urlpatterns += static('favicon.ico', document_root=web_root)
-    urlpatterns += static('robots.txt', document_root=web_root)
-    urlpatterns += static('humans.txt', document_root=web_root)
-    urlpatterns += static(settings.STATIC_URL)
