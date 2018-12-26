36084f621028e13a0ba589824d86d9fc0f6c5739
Maxim Oransky <maxim.oransky@gmail.com>
2018-12-26 22:39:06 +0300

    во-первых, фиксим статику

--- demobaza/urls.py
+++ demobaza/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
 
 from . import views
 
@@ -9,7 +9,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+
 if settings.DEBUG:
+    import os
     from django.conf.urls.static import static
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    web_root = os.path.join(settings.BASE_DIR, 'web')
+    urlpatterns += static('favicon.ico', document_root=web_root)
+    urlpatterns += static('robots.txt', document_root=web_root)
+    urlpatterns += static('humans.txt', document_root=web_root)
     urlpatterns += static(settings.STATIC_URL)
