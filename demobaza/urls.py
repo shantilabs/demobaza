3646dc9e749dc0a80b0582f9f97af9e912afed75
Maxim Oransky <maxim.oransky@gmail.com>
2018-12-26 03:46:47 +0300

    заглушка для главной страницы

--- demobaza/urls.py
+++ demobaza/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from . import views
+
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL)
