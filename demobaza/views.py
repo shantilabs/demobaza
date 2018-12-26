a6e22c8b89e9cdd5b8c89841f249ae16e0a868d1
Maxim Oransky <maxim.oransky@gmail.com>
2018-12-26 23:10:43 +0300

    рефакторинг

--- demobaza/views.py
+++ demobaza/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from demobaza.models import Project
+
+from .models import Project
 
 
 def home(request):
-    project_data = Project.objects.filter(is_active=True)
-    return render(request, 'demobaza/home.html', context={'projects': project_data})
+    return render(request, 'demobaza/templates/home.html', {
+        'projects': Project.objects.filter(is_active=True),
+    })
